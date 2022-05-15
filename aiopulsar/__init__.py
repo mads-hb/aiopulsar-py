0e93c5bb22e6d0aa4dc28974691ee1823491d777
Mads Hansen Baattrup <mhb@danskecommodities.com>
2022-05-15 19:47:50 +0200

    ✨ Add types to __init__.py

--- aiopulsar/__init__.py
+++ aiopulsar/__init__.py
@@ -1,3 +1,8 @@
 from aiopulsar.client import connect
+from aiopulsar.consumer import Consumer
+from aiopulsar.producer import Producer
+from aiopulsar.reader import Reader
+from aiopulsar.client import Client
 
-__all__ = ["connect"]
+
+__all__ = ["connect", "Consumer", "Producer", "Reader", "Client"]
